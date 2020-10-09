d48b4fc22a653625c6c1e1c0143d8694bfa4453b
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-10-09 17:57:36 +0300

    feat: connect to redux to load products data

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,5 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { loadProducts } from "../redux/actions";
 
-export default function ProductsPage(props) {
-  return <div>ProductsPage</div>;
+function ProductsPage({ loadProducts, products }) {
+  useEffect(() => {
+    const fetchProducts = async () => await loadProducts();
+    fetchProducts();
+  }, []);
+
+  const productList = Object.entries(products).map(([id, product]) => (
+    <li key={id}>{product.title}</li>
+  ));
+
+  return (
+    <div>
+      <ul>{productList}</ul>
+    </div>
+  );
 }
+
+const mapStateToProps = ({ products }) => ({ products });
+
+export default connect(mapStateToProps, { loadProducts })(ProductsPage);
