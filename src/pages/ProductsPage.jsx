7c922dbdaef43d0286011f6c53baa82f6cee9e1f
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-10-12 15:34:04 +0300

    feat: add product cards

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,20 +1,30 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { loadProducts } from "../redux/actions";
+import "./ProductPage.scss";
 
 function ProductsPage({ loadProducts, products }) {
   useEffect(() => {
     const fetchProducts = async () => await loadProducts();
     fetchProducts();
-  }, []);
+  }, [loadProducts]);
 
   const productList = Object.entries(products).map(([id, product]) => (
-    <li key={id}>{product.title}</li>
+    <li key={id} className='product-card'>
+      <div className='product-card__img-box'>
+        <img src={`./${product.imageUrl}`} alt={product.title} />
+      </div>
+      <div className='product-card__footer'>
+        <p className='product-title'>{product.title}</p>
+        <p className='product-price'>{product.price}</p>
+        <button>Buy</button>
+      </div>
+    </li>
   ));
 
   return (
     <div>
-      <ul>{productList}</ul>
+      <ul className='product-list'>{productList}</ul>
     </div>
   );
 }
