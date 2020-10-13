39dac4e4b9811a79747d4afeacadc408dd2be00d
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-10-13 19:49:47 +0300

    feat: implement cart functionality

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,38 @@
 import React from "react";
+import { connect } from "react-redux";
+import { edit, remove, clear } from "../redux/actions";
+import CartItem from "./CartItem";
 import "./Cart.scss";
 
-export default function Cart(props) {
-  return <div className='Cart'>Cart</div>;
+function Cart({ cart, products, edit, remove, clear }) {
+  const cartItems = cart.keys.map((id) => (
+    <CartItem
+      key={id}
+      id={id}
+      {...cart.products[id]}
+      {...products[id]}
+      edit={edit}
+      remove={remove}
+    />
+  ));
+
+  const cartSum = cart.keys.reduce(
+    (sum, id) => sum + products[id].price * cart.products[id].num,
+    0
+  );
+
+  return (
+    <div className='Cart'>
+      <p>Cart</p>
+      <ul>{cartItems}</ul>
+      <p>{`Cart total: ${cartSum}`}</p>
+      <p>
+        <button onClick={clear}>Clear cart</button>
+      </p>
+    </div>
+  );
 }
+
+const mapStateToProps = ({ cart, products }) => ({ cart, products });
+
+export default connect(mapStateToProps, { edit, remove, clear })(Cart);
