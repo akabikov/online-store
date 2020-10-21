6c69d7464485a034d5dbdeb05288020e089ff89e
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-10-21 14:09:51 +0300

    refactor: move validation to filters

--- src/redux/selectors/filters.js
+++ src/redux/selectors/filters.js
@@ -1,22 +1,14 @@
 const filters = {
-  company: {
-    validateFilter: (companyList) => companyList?.size > 0,
-    applyFilter: (companyList, product) => companyList.has(product?.company),
-  },
-  price: {
-    validateFilter: (price) => price?.max > 0 && price?.min <= price?.max,
-    applyFilter: ({ min = 0, max = Infinity }, product) =>
-      +product?.price >= min && +product?.price <= max,
-  },
-  search: {
-    validateFilter: (query) => query?.length > 0,
-    applyFilter: (query, product) => product?.title.includes(query),
-  },
+  company: (companyList, { company }) =>
+    companyList.has(company) || companyList.size > 0,
+  price: ({ min = 0, max = Infinity }, { price }) =>
+    +price >= min && +price <= max,
+  search: (query, { title }) => title.includes(query),
 };
 
 export const applyFilters = (rules, product) =>
   Object.entries(rules).reduce(
     (commonCondition, [ruleName, ruleValue]) =>
-      commonCondition && filters[ruleName]?.applyFilter(ruleValue, product),
+      commonCondition && filters[ruleName]?.(ruleValue, product),
     true
   );
