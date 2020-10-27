2d600cf374a493d303cd01a49b50dd0acc9bfd21
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-10-27 20:22:51 +0300

    refactor: move state to parent component

--- src/components/ProductFilters/PriceFilter/index.jsx
+++ src/components/ProductFilters/PriceFilter/index.jsx
@@ -1,45 +1,39 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import { getFilteredProductsPriceRange } from "../../../redux/selectors";
 import "./style.scss";
 
-const INIT_STATE = { min: "", max: "" };
-
-function PriceFilter({ availableRange, updateFilters }) {
-  const [price, setPrice] = useState(INIT_STATE);
-
-  useEffect(() => {
-    updateFilters({
-      price: { min: Number(price.min), max: Number(price.max) },
-    });
-  }, [price, updateFilters]);
-
+function PriceFilter({ availableRange, price, setFilter }) {
   return (
     <fieldset>
       <legend>Price</legend>
       <label htmlFor='price-min'>from</label>
       <input
         type='number'
-        name='price-min'
         id='price-min'
         min='0'
-        placeholder={availableRange.min || ""}
-        value={price.min}
-        onChange={({ target: { value } }) => setPrice({ ...price, min: value })}
+        placeholder={availableRange?.min || ""}
+        value={price?.min || ""}
+        onChange={({ target: { value } }) =>
+          setFilter("price", { ...price, min: value })
+        }
       />
       <label htmlFor='price-max'>to</label>
       <input
         type='number'
-        name='price-max'
         id='price-max'
         min='0'
-        placeholder={availableRange.max || ""}
-        value={price.max}
-        onChange={({ target: { value } }) => setPrice({ ...price, max: value })}
+        placeholder={availableRange?.max || ""}
+        value={price?.max || ""}
+        onChange={({ target: { value } }) =>
+          setFilter("price", { ...price, max: value })
+        }
       />
-      <button type='reset' onClick={() => setPrice(INIT_STATE)}>
-        Reset
-      </button>
+      <input
+        type='button'
+        value='Reset'
+        onClick={() => setFilter("price", {})}
+      />
     </fieldset>
   );
 }
