84e90284974cc5dc4417a783af055e24e8693734
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-10-28 17:42:02 +0300

    feat: add filtering debounce

--- src/components/ProductFilters/index.jsx
+++ src/components/ProductFilters/index.jsx
@@ -1,18 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import { connect } from "react-redux";
 import { updateFilters } from "../../redux/actions";
 import { getFilters } from "../../redux/selectors";
+import debounce from "../../helpers/debounce";
 import SearchFilter from "./SearchFilter";
 import PriceFilter from "./PriceFilter";
 import BrandFilter from "./BrandFilter";
 import "./style.scss";
 
+const FILTERING_DELAY = 1000; // ms
+
 function ProductFilters({ storeFilters, updateFilters }) {
   const [filters, setFilters] = useState(storeFilters);
 
+  const debouncedFilterUpdate = useCallback(
+    debounce((filters) => updateFilters(filters), FILTERING_DELAY),
+    []
+  );
+
   useEffect(() => {
-    updateFilters(filters);
-  }, [filters, updateFilters]);
+    debouncedFilterUpdate(filters);
+  }, [filters, debouncedFilterUpdate]);
 
   const setFilter = (filterName, filterValue) =>
     setFilters((filters) => ({ ...filters, [filterName]: filterValue }));
