ec9dfea8e79230f111d79c15f1cc951ea24db3ca
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-10-30 17:38:57 +0300

    feat: add pagination selector

--- src/redux/selectors/index.js
+++ src/redux/selectors/index.js
@@ -42,3 +42,18 @@ export const getSortedProducts = createSelector(
   (products, sorting) =>
     [...products].sort((current, next) => applySorting(current, next, sorting))
 );
+
+export const getPaginatedProducts = createSelector(
+  getSortedProducts,
+  (_, { itemsPerPage }) => itemsPerPage,
+  (_, { pageNum }) => pageNum,
+  (products, itemsPerPage, pageNum = 1) => {
+    const safeItemsPerPage = itemsPerPage || products.length;
+    const numOfPages = Math.ceil(products.length / safeItemsPerPage);
+    const safePageNum =
+      pageNum > numOfPages ? numOfPages : pageNum < 1 ? 1 : pageNum;
+    const start = (safePageNum - 1) * safeItemsPerPage;
+    const productsOfPage = products.slice(start, start + safeItemsPerPage);
+    return { products: productsOfPage, numOfPages };
+  }
+);
