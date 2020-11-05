5093d6b7d7244751413e6bd19b256e3f0ab65bee
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-11-05 18:01:08 +0300

    feat: add single product page

--- src/pages/SingleProductPage.jsx
+++ src/pages/SingleProductPage.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { getProductById } from "../redux/selectors";
+import { add } from "../redux/actions";
 
-export default function SingleProductPage({ src }) {
-  return <div>{`SingleProductPage of ${src}`}</div>;
+export default function SingleProductPage({ src: id }) {
+  const { title, price, company, description, imageUrl } =
+    useSelector((state) => getProductById(state, id)) || {};
+  const dispatch = useDispatch();
+  const handleAdd = () => dispatch(add(id));
+  const history = useHistory();
+  const handleBack = () => history.push("/products");
+
+  return (
+    <section>
+      <div>
+        <img src={`/${imageUrl}`} alt={title} />
+      </div>
+      <div>
+        <p>{title}</p>
+        <p>{company}</p>
+        <p>{price}</p>
+        <p>{description}</p>
+        <button onClick={handleAdd}>Add to cart</button>
+        <button onClick={handleBack}>Back to products</button>
+      </div>
+    </section>
+  );
 }
