b3378ecd0df5d4cbb47965181b7ba7d7d2a11c78
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-11-05 13:02:04 +0300

    feat: add a selector for a product by id

--- src/redux/selectors/index.js
+++ src/redux/selectors/index.js
@@ -7,6 +7,8 @@ const getProductsArray = createSelector(getProducts, (products) =>
   Object.entries(products).map(([id, product]) => ({ id, ...product }))
 );
 
+export const getProductById = (state, id) => getProducts(state)?.[id];
+
 export const getCompanies = createSelector(getProductsArray, (products) =>
   Array.from(new Set(products.map(({ company }) => company))).sort()
 );
