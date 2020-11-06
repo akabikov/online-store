da11ef9848fdbdd42e86d98257bfedc97aed227a
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-11-06 21:03:03 +0300

    refactor: move get/set/push query functions to custom hook

--- src/hooks/useQuery.js
+++ src/hooks/useQuery.js
@@ -1,7 +1,23 @@
-import { useLocation } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 function useQuery() {
-  return new URLSearchParams(useLocation().search);
+  const history = useHistory();
+  const getParams = () => new URLSearchParams(history.location.search);
+
+  return {
+    get(key) {
+      return getParams().get(key);
+    },
+    set(newParams) {
+      const params = getParams();
+      for (const [key, value] of Object.entries(newParams))
+        params.set(key, value);
+      return { search: `?${params}` };
+    },
+    push(newParams) {
+      history.push(this.set(newParams));
+    },
+  };
 }
 
 export default useQuery;
