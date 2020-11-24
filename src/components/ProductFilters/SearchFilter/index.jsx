69a647830221af087f380e9b33ba3bdab55cba98
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-11-24 13:04:45 +0300

    fix: add placeholder

--- src/components/ProductFilters/SearchFilter/index.jsx
+++ src/components/ProductFilters/SearchFilter/index.jsx
@@ -6,11 +6,12 @@ const FILTER_NAME = "search";
 function SearchFilter({ search, setFilter }) {
   return (
     <div>
-      <label htmlFor='search'>Search</label>
       <input
         type='search'
         name='search'
         id='search'
+        placeholder='Search...'
+        inputMode='search'
         value={search?.query || ""}
         onChange={({ target: { value } }) =>
           setFilter(FILTER_NAME, { query: value })
