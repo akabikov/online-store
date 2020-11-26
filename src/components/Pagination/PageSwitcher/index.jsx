6a113fce2521a023429ac56c702f081bdcf6eb3c
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-11-26 19:52:13 +0300

    style: add titles for links

--- src/components/Pagination/PageSwitcher/index.jsx
+++ src/components/Pagination/PageSwitcher/index.jsx
@@ -3,13 +3,14 @@ import { NavLink } from "react-router-dom";
 import "./style.scss";
 
 function PageSwitcher({ numOfPages, currentPage, switchPage }) {
-  const pageLink = (page, title) => (
+  const pageLink = (page, title, icon) => (
     <NavLink
       to={switchPage(page)}
       isActive={() => currentPage === page}
-      activeStyle={{ textDecoration: "none" }}
+      activeClassName='current-page'
+      title={currentPage !== page && title}
     >
-      {title || page}
+      {icon || page}
     </NavLink>
   );
 
@@ -19,20 +20,21 @@ function PageSwitcher({ numOfPages, currentPage, switchPage }) {
       const i = idx + 1;
       return (
         <li key={i}>
-          {pageLink(i)}
+          {pageLink(i, `Go to page ${i}`)}
           {i === numOfPages || ", "}
         </li>
       );
     });
 
-  const first = currentPage > 1 && <li>{pageLink(1, "<<")}</li>;
-  const prev = currentPage > 1 && <li>{pageLink(currentPage - 1, "<")}</li>;
-  const next = currentPage < numOfPages && (
-    <li>{pageLink(currentPage + 1, ">")}</li>
-  );
-  const last = currentPage < numOfPages && (
-    <li>{pageLink(numOfPages, ">>")}</li>
-  );
+  const getEdgeLink = (excludedPage, name, page, icon) =>
+    Boolean(currentPage - excludedPage) && (
+      <li>{pageLink(page, `Go to the ${name} page`, icon)}</li>
+    );
+
+  const first = getEdgeLink(1, "first", 1, "<<");
+  const prev = getEdgeLink(1, "previous", currentPage - 1, "<");
+  const next = getEdgeLink(numOfPages, "next", currentPage + 1, ">");
+  const last = getEdgeLink(numOfPages, "last", numOfPages, ">>");
 
   return (
     <ul className='PageSwitcher'>
