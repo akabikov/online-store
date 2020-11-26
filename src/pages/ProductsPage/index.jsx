4df201b16926522a20b3ba6e8d0f4ec5e0b02670
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-11-26 18:11:48 +0300

    refactor: move to own dir and add style file

---
+++ src/pages/ProductsPage/index.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import ProductFilters from "../../components/ProductFilters";
+import ProductsSorting from "../../components/ProductsSorting";
+import ProductList from "../../components/ProductList";
+import ItemsPerPageSelector from "../../components/Pagination/ItemsPerPageSelector";
+import PageSwitcher from "../../components/Pagination/PageSwitcher";
+import { getPaginatedProducts } from "../../redux/selectors";
+import usePagination from "../../hooks/usePagination";
+import "./style.scss";
+
+function ProductsPage() {
+  const {
+    itemsPerPage,
+    setItemsPerPage,
+    numOfPages,
+    page,
+    switchPage,
+    products,
+  } = usePagination(getPaginatedProducts);
+
+  return (
+    <div>
+      <ProductFilters />
+      <div className='product-list-header'>
+        <ProductsSorting />
+        <ItemsPerPageSelector
+          itemsPerPage={itemsPerPage}
+          setItemsPerPage={setItemsPerPage}
+          listOfOptions={[3, 5, 10, 20, 0]}
+        />
+      </div>
+      <ProductList products={products} />
+      <PageSwitcher
+        numOfPages={numOfPages}
+        currentPage={page}
+        switchPage={switchPage}
+      />
+    </div>
+  );
+}
+
+export default ProductsPage;
