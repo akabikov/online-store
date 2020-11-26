ff22e8064cae442599243b7ce93e1076599f965c
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-11-26 17:49:11 +0300

    refactor: move to own dir and add style file

--- src/components/Pagination/PageSwitcher/index.jsx
+++ src/components/Pagination/PageSwitcher/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "./style.scss";
 
 function PageSwitcher({ numOfPages, currentPage, switchPage }) {
   const pageLink = (page, title) => (
