56b1c22b30e8764444976d576d5e5be7d28a40bd
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-11-27 16:18:13 +0300

    style: add styles for the page switcher

--- src/components/Pagination/PageSwitcher/index.jsx
+++ src/components/Pagination/PageSwitcher/index.jsx
@@ -1,44 +1,55 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./style.scss";
 
 function PageSwitcher({ numOfPages, currentPage, switchPage }) {
   const pageLink = (page, title, icon) => (
-    <NavLink
+    <Link
       to={switchPage(page)}
-      isActive={() => currentPage === page}
-      activeClassName='current-page'
-      title={currentPage !== page && title}
+      title={currentPage !== page ? title : undefined}
     >
       {icon || page}
-    </NavLink>
+    </Link>
   );
 
   const pages =
     numOfPages > 1 &&
-    [...Array(numOfPages)].map((_, idx) => {
-      const i = idx + 1;
+    [...Array(numOfPages)].map((_, i) => {
+      const page = i + 1;
       return (
-        <li key={i}>
-          {pageLink(i, `Go to page ${i}`)}
-          {i === numOfPages || ", "}
+        <li
+          key={page}
+          className={page === currentPage ? "current-page" : undefined}
+        >
+          {pageLink(page, `Go to page ${page}`)}
         </li>
       );
     });
 
-  const getEdgeLink = (excludedPage, name, page, icon) =>
-    Boolean(currentPage - excludedPage) && (
-      <li>{pageLink(page, `Go to the ${name} page`, icon)}</li>
-    );
+  const getEdgeLink = (excludedPage, name, page, icon) => (
+    <li className={!(currentPage - excludedPage) ? "disabled" : undefined}>
+      {pageLink(page, `Go to the ${name} page`, icon)}
+    </li>
+  );
 
-  const first = getEdgeLink(1, "first", 1, "<<");
-  const prev = getEdgeLink(1, "previous", currentPage - 1, "<");
-  const next = getEdgeLink(numOfPages, "next", currentPage + 1, ">");
-  const last = getEdgeLink(numOfPages, "last", numOfPages, ">>");
+  const prev = getEdgeLink(
+    1,
+    "previous",
+    currentPage - 1,
+    <i className='zmdi zmdi-chevron-left'></i>
+  );
+  const next = getEdgeLink(
+    numOfPages,
+    "next",
+    currentPage + 1,
+    <i className='zmdi zmdi-chevron-right'></i>
+  );
+  // const first = getEdgeLink(1, "first", 1, "<<");
+  // const last = getEdgeLink(numOfPages, "last", numOfPages, ">>");
 
   return (
     <ul className='PageSwitcher'>
-      {first} {prev} {pages} {next} {last}
+      {prev} {pages} {next}
     </ul>
   );
 }
