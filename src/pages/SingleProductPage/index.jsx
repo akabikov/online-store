904acc8ae53d3896757c23a7105d3fe3c9a9cc10
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-12-03 16:06:36 +0300

    style: add styles for single product page

--- src/pages/SingleProductPage/index.jsx
+++ src/pages/SingleProductPage/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams, useHistory } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { getProductById } from "../../redux/selectors";
 import { add } from "../../redux/actions";
@@ -11,21 +11,36 @@ export default function SingleProductPage() {
     useSelector((state) => getProductById(state, id)) || {};
   const dispatch = useDispatch();
   const handleAdd = () => dispatch(add(id));
-  const history = useHistory();
-  const handleBack = () => history.push("/products");
 
   return (
-    <section>
-      <div>
+    <section className='SingleProductPage'>
+      <div className='product-img'>
         <img src={`/${imageUrl}`} alt={title} />
       </div>
-      <div>
-        <p>{title}</p>
-        <p>{company}</p>
-        <p>{price}</p>
-        <p>{description}</p>
-        <button onClick={handleAdd}>Add to cart</button>
-        <button onClick={handleBack}>Back to products</button>
+      <div className='product-info'>
+        <div className='product-info__main'>
+          <h2 className='product-info__title'>{title}</h2>
+          <div className='product-info__company'>
+            <h3>Company:</h3>
+            <span> {company}</span>
+          </div>
+          <div className='product-info__price'>
+            <h3>Price:</h3>
+            <span> ${price}</span>
+          </div>
+          <div className='product-info__description'>
+            <h3>About product: </h3>
+            <p>{description}</p>
+          </div>
+        </div>
+        <div className='product-info__footer'>
+          <button className='product-info__add-button' onClick={handleAdd}>
+            Add to cart
+          </button>
+          <Link to='/products' className='product-info__back-button'>
+            Back to products
+          </Link>
+        </div>
       </div>
     </section>
   );
