823169fd8cf24b12fcd71f28d25e89a996df6e16
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-12-04 17:15:35 +0300

    feat: add content and styles to contact page

--- src/pages/ContactPage.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function ContactPage(props) {
-  return <div>ContactPage</div>;
-}
---
+++ src/pages/ContactPage/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import useToggleState from "../../hooks/useToggleState";
+import "./style.scss";
+
+export default function ContactPage() {
+  const [isSubmitted, submit] = useToggleState();
+
+  return (
+    <section className='ContactPage'>
+      {isSubmitted ? (
+        <div className='ContactPage__success-message'>
+          <p>Thank you for getting in touch!</p>
+          <p>One of our colleagues will get back in touch with you soon!</p>
+          <p>Have a great day!</p>
+        </div>
+      ) : (
+        <form className='ContactPage__contact-form' onSubmit={submit}>
+          <input type='text' name='name' placeholder='Name' />
+          <input type='email' name='email' required placeholder='Email' />
+          <input type='text' name='subject' placeholder='Subject' />
+          <textarea
+            name='message'
+            required
+            rows='10'
+            placeholder='Your message...'
+          ></textarea>
+          <input type='submit' value='Send' />
+        </form>
+      )}
+    </section>
+  );
+}
