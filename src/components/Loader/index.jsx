4010329e32984bab735666505853dd50ddfa7046
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-12-07 14:15:42 +0300

    feat: add loader

---
+++ src/components/Loader/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.scss";
+
+function Loader() {
+  return (
+    <div className='Loader'>
+      <i className='zmdi zmdi-spinner zmdi-hc-5x zmdi-hc-spin'></i>
+    </div>
+  );
+}
+
+export default Loader;
