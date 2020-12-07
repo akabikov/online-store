fc6f21980664db437469718046c2cf041c8d3e28
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-12-07 13:36:08 +0300

    feat: add cart page items

--- src/pages/CartPage/index.jsx
+++ src/pages/CartPage/index.jsx
@@ -1,6 +1,54 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { edit, remove, clear } from "../../redux/actions";
+import CartItem from "../../components/Cart/CartItem";
 import "./style.scss";
 
-export default function CartPage(props) {
-  return <div>CartPage</div>;
+export default function CartPage() {
+  const { cart, products } = useSelector(({ cart, products }) => ({
+    cart,
+    products,
+  }));
+  const dispatch = useDispatch();
+
+  const cartItems = cart.keys.map((id) => (
+    <CartItem
+      key={id}
+      id={id}
+      {...cart.products[id]}
+      {...products[id]}
+      edit={() => dispatch(edit)}
+      remove={() => dispatch(remove)}
+    />
+  ));
+
+  const cartSum = cart.keys.reduce(
+    (sum, id) => sum + products[id].price * cart.products[id].num,
+    0
+  );
+
+  return (
+    <div>
+      {cartItems.length ? (
+        <>
+          <ul>{cartItems}</ul>
+          <div className='cart__total'>
+            <span> {"Cart total: "}</span>
+            <span className='cart__total-sum'>{cartSum}</span>
+          </div>
+          <button className='cart__button' onClick={() => dispatch(clear)}>
+            Clear cart
+          </button>
+        </>
+      ) : (
+        <>
+          <div>Cart is empty</div>
+          <Link to='/products' className='cart__button'>
+            View products
+          </Link>
+        </>
+      )}
+    </div>
+  );
 }
