e5ed931210bce2590aeeee2a452dcaa7b0c57e83
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-12-11 16:30:16 +0300

    feat: add inverted style

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -4,14 +4,23 @@ import "./style.scss";
 
 const NativeButton = (props) => <button {...props}>{props.children}</button>;
 
-function Button({ text = "", link, icon, type = "button", ...props }) {
+function Button({
+  text = "",
+  link,
+  icon,
+  type = "button",
+  inverted = false,
+  ...props
+}) {
   const isButton = !link;
   const Btn = isButton ? NativeButton : Link;
 
   return (
     <Btn
       to={link}
-      className={"button" + (icon ? " button-icon" : "")}
+      className={
+        "button" + (icon ? " button-icon" : "") + (inverted ? " inverted" : "")
+      }
       type={isButton ? type : undefined}
       {...props}
     >
