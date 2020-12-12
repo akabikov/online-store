1351b5f88ea6046f1ce2e005c0d4e7fa90655a3e
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-12-12 19:29:14 +0300

    feat: add the about page

--- src/pages/AboutPage.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function AboutPage(props) {
-  return <div>AboutPage</div>;
-}
---
+++ src/pages/AboutPage/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./style.scss";
+
+function AboutPage() {
+  return (
+    <section className='AboutPage'>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur,
+        voluptatibus repellendus quidem aperiam ratione, velit, facilis magnam
+        repellat dolor odio ullam. Harum, officia voluptate commodi eligendi
+        odio nulla! Dignissimos, consectetur. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Consequatur, voluptatibus repellendus
+        quidem aperiam ratione, velit, facilis magnam repellat dolor odio ullam.
+        Harum, officia voluptate commodi eligendi odio nulla! Dignissimos,
+        consectetur. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Consequatur, voluptatibus repellendus quidem aperiam ratione, velit,
+        facilis magnam repellat dolor odio ullam. Harum, officia voluptate
+        commodi eligendi odio nulla! Dignissimos, consectetur. Lorem ipsum dolor
+        sit amet consectetur adipisicing elit. Consequatur, voluptatibus
+        repellendus quidem aperiam ratione, velit, facilis magnam repellat dolor
+        odio ullam. Harum, officia voluptate commodi eligendi odio nulla!
+        Dignissimos, consectetur. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Consequatur, voluptatibus repellendus quidem aperiam
+        ratione, velit, facilis magnam repellat dolor odio ullam. Harum, officia
+        voluptate commodi eligendi odio nulla! Dignissimos, consectetur.
+      </p>
+    </section>
+  );
+}
+
+export default AboutPage;
