410462ad42d26c052891bab988133b28a1dd39b4
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-12-12 18:56:10 +0300

    refactor: move the success message to a separate page

--- src/pages/ContactPage/index.jsx
+++ src/pages/ContactPage/index.jsx
@@ -1,33 +1,29 @@
 import React from "react";
-import useToggleState from "../../hooks/useToggleState";
+import { useHistory } from "react-router-dom";
 import Button from "../../components/Button";
 import "./style.scss";
 
 export default function ContactPage() {
-  const [isSubmitted, submit] = useToggleState();
+  const history = useHistory();
+  const submit = (e) => {
+    e.preventDefault();
+    history.push("/success");
+  };
 
   return (
     <section className='ContactPage'>
-      {isSubmitted ? (
-        <div className='ContactPage__success-message'>
-          <p>Thank you for getting in touch!</p>
-          <p>One of our colleagues will get back in touch with you soon!</p>
-          <p>Have a great day!</p>
-        </div>
-      ) : (
-        <form className='ContactPage__contact-form' onSubmit={submit}>
-          <input type='text' name='name' placeholder='Name' />
-          <input type='email' name='email' required placeholder='Email' />
-          <input type='text' name='subject' placeholder='Subject' />
-          <textarea
-            name='message'
-            required
-            rows='10'
-            placeholder='Your message...'
-          ></textarea>
-          <Button type='submit' text='Send' />
-        </form>
-      )}
+      <form className='ContactPage__contact-form' onSubmit={submit}>
+        <input type='text' name='name' placeholder='Name' />
+        <input type='email' name='email' required placeholder='Email' />
+        <input type='text' name='subject' placeholder='Subject' />
+        <textarea
+          name='message'
+          required
+          rows='10'
+          placeholder='Your message...'
+        ></textarea>
+        <Button type='submit' text='Send' />
+      </form>
     </section>
   );
 }
