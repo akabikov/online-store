28cced41cc598b9848e187e9608d96375a6f7d64
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-12-14 00:09:13 +0300

    refactor: move the page component to its own dir

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.scss";
+
+function HomePage() {
+  return (
+    <section className='HomePage'>
+      <div>Home page</div>
+    </section>
+  );
+}
+
+export default HomePage;
