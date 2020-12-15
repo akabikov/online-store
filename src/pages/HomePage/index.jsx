7dc74dab19ece55ab682525e5b56e89f7796063f
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-12-15 14:11:23 +0300

    feat: add content and styles to the home page

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,10 +1,32 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { getFeaturedProducts } from "../../redux/selectors";
+import ProductList from "../../components/ProductList";
+import Button from "../../components/Button";
 import "./style.scss";
 
 function HomePage() {
+  const products = useSelector(getFeaturedProducts);
   return (
     <section className='HomePage'>
-      <div>Home page</div>
+      <div
+        className='hero'
+        style={{ backgroundImage: `url(/media/static/singleProductBcg.jpeg)` }}
+      >
+        <div className='banner'>
+          <h1>classic devices</h1>
+          <div className='slogan'>Vintage, not obsolete!</div>
+          <div className='banner__link'>
+            <Button text='Get classic' link='/products' inverted />
+          </div>
+        </div>
+      </div>
+
+      <div className='featured-products'>
+        <h2 className='featured-products__title'>Featured products</h2>
+        <ProductList products={products} />
+        <Button text='View all products' link='/products' />
+      </div>
     </section>
   );
 }
